a0e6ed8962f848684bcc5f2f153e457b134910a5
bloshko_m <maksbloshko@gmail.com>
2021-07-12 17:13:01 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const employeeRoutes = require('./routes/employees');
+const keys = require('./config/keys');
+const app = express();
+
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('mongoDB connected'))
+  .catch(error => console.log(error));
+
+app.use(require('morgan')('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(require('cors')());
+app.use('/api/employees', employeeRoutes);
+
+module.exports = app;
